ae7c76678904c00c71bf7c8af58e9372768f5570
dzakarya <muhammadzakaria181094@gmail.com>
2022-07-25 14:05:41 +0700

    change light routes prefix

--- app/routes/api.py
+++ app/routes/api.py
@@ -3,5 +3,5 @@ from .v1 import endpoint_light,endpoint_temp
 
 api_router = APIRouter()
 
-api_router.include_router(endpoint_light.router, tags=['ligth'], prefix='/ligth')
+api_router.include_router(endpoint_light.router, tags=['light'], prefix='/light')
 api_router.include_router(endpoint_temp.router, tags=['temp'], prefix='/temp')
