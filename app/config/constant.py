fd40ef187263225e8f1134e16ccb52636490aae8
dzakarya <zakaria@koinworks.com>
2022-07-28 17:50:44 +0700

    add charset 2

--- app/config/constant.py
+++ app/config/constant.py
@@ -1,10 +1,10 @@
 import pytz
 
-temp_topic = "temp_topic"
-light_topic = "light_topic"
-smoke_topic = "smoke_topic"
-db_name = "master"
-db_user = "root"
-db_host = "localhost"
-db_password = "teknologisatujaman"
-tz = pytz.timezone("Asia/Jakarta")
+temp_topic = 'temp_topic'
+light_topic = 'light_topic'
+smoke_topic = 'smoke_topic'
+db_name = 'master'
+db_user = 'root'
+db_host = 'localhost'
+db_password = 'teknologisatujaman'
+tz = pytz.timezone('Asia/Jakarta')
