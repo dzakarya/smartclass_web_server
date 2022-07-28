b666e00c06a61b56e120ffa3bb847acea07cf814
dzakarya <muhammadzakaria181094@gmail.com>
2022-07-28 01:34:26 -0400

    fix routes

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from .v1 import endpoint_light,endpoint_temp
+from .v1 import endpoint_light,endpoint_temp,endpoint_smoke
 
 api_router = APIRouter()
 
 api_router.include_router(endpoint_light.router, tags=['light'], prefix='/light')
 api_router.include_router(endpoint_temp.router, tags=['temp'], prefix='/temp')
+api_router.include_router(endpoint_smoke.router, tags=['smoke'],prefix='/smoke')
