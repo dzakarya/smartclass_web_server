602c5fbbd21e2e8e83919322dd4325d30f859fa2
dzakarya <zakaria@koinworks.com>
2022-08-05 16:30:18 +0700

    add endpoint_data log4

--- app/routes/v1/endpoint_datalog.py
+++ app/routes/v1/endpoint_datalog.py
@@ -4,8 +4,9 @@ from ...schemas.api_request import DatalogRequest
 router = APIRouter()
 
 @router.post("/get-data")
-async def get_data_log(req):
-    logger.info(req)
+async def get_data(req : DatalogRequest):
+    logger.info(req.date)
+    logger.info(req.time)
     # return ResponseGetValue(
     #     data={
     #         "value":value
