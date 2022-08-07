3702979fce64b90b8b6abeb5aa43f98669894e48
dzakarya <zakaria@koinworks.com>
2022-08-07 22:53:24 +0700

    return to mqtt schema light

--- app/repositories/light.py
+++ app/repositories/light.py
@@ -1,26 +1,16 @@
 from .mqtt import mqtt
 from ..config.constant import light_topic
-import RPi.GPIO as GPIO
-from loguru import logger
 
-class LightRepository():
-    def __init__(self) -> None:
-        self._zone1_pin = 12
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self._zone1_pin,GPIO.OUT)
-        self._pwm_zone1 = GPIO.PWM(self._zone1_pin,1000)
-        self._zone1_value = 0
-        self._pwm_zone1.start(self._zone1_value)
+def set_light(zone1 : float, zone2 : float):
+    try:
+        result = mqtt.publish(f'out_{light_topic}', f"zone1:{zone1}-zone2:{zone2}")
+    except Exception as e:
+        return e
+    return result
 
-    def set_light(self,value : int):
-        try:
-            self._pwm_zone1.ChangeDutyCycle(value)
-        except Exception as e:
-            logger.error(e)
-
-    def get_light():
-        try:
-            result = mqtt.light
-        except Exception as e:
-            return e
-        return result
+def get_light():
+    try:
+        result = mqtt.light
+    except Exception as e:
+        return e
+    return result
--- app/routes/v1/endpoint_light.py
+++ app/routes/v1/endpoint_light.py
@@ -1,26 +1,25 @@
 from fastapi import APIRouter
 from loguru import logger
 
-from app.repositories.light import LightRepository
+from app.repositories.light import get_light,set_light
 from ...schemas.api_response import ResponseGetLightValue
 from ...schemas.api_request import LightRequest
 router = APIRouter()
 
-Lrep = LightRepository()
 @router.get("/get-light",response_model=ResponseGetLightValue)
 async def gettempvalue():
-    Lrep.get_light()
+    value = get_light()
     return ResponseGetLightValue(
         data={
-            "zone1":0,
-            "zone2":0
+            "zone1":value,
+            "zone2":value
         },
         meta=None
     )
 
 @router.post("/set-light")
 async def setlightvalue(req: LightRequest):
-    Lrep.set_light(req.zone1)
+    result = set_light(req.zone1,req.zone2)
     return{
         "zone1":req.zone1,
         "zone2":req.zone2
