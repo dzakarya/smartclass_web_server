d52af68a54b668f819b07536a818f3746583beec
dzakarya <zakaria@koinworks.com>
2022-08-08 02:08:02 +0700

    fix query7

--- app/model/sql/db.py
+++ app/model/sql/db.py
@@ -33,4 +33,4 @@ class DB():
         query = f"""SELECT * FROM {tab_name} where date = %s and time = %s"""
         val = (date,hour)
         cur.execute(query,val)
-        return cur.fetchone()
+        return cur.fetchone()["value"]
