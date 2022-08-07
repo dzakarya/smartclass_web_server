8a3872217e7f63f5e47e77803a26579af870bc46
dzakarya <zakaria@koinworks.com>
2022-08-08 01:28:34 +0700

    fix date3

--- app/routes/v1/endpoint_datalog.py
+++ app/routes/v1/endpoint_datalog.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
 from loguru import logger
+from ...repositories.datalog import get_current_data
 from ...schemas.api_request import DatalogRequest
 router = APIRouter()
 
 @router.post("/get-data")
 async def get_data(req : DatalogRequest):
-    logger.info(req.date)
-    logger.info(req.time)
+    get_current_data(req.date, req.time)
     # return ResponseGetValue(
     #     data={
     #         "value":value
