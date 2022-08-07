f5fcf387dbc586ff81b905ed870c183b7737be20
dzakarya <zakaria@koinworks.com>
2022-08-07 21:31:36 +0700

    add pwm fix

--- app/routes/v1/endpoint_light.py
+++ app/routes/v1/endpoint_light.py
@@ -1,26 +1,27 @@
 from fastapi import APIRouter
 from loguru import logger
 
-from app.repositories.light import get_light
+from app.repositories.light import LightRepository
 from ...schemas.api_response import ResponseGetLightValue
 from ...schemas.api_request import LightRequest
 router = APIRouter()
 
 @router.get("/get-light",response_model=ResponseGetLightValue)
 async def gettempvalue():
-    value = get_light()
+    Lrep = LightRepository()
+    Lrep.get_light()
     return ResponseGetLightValue(
         data={
-            "zone1":value,
-            "zone2":value
+            "zone1":0,
+            "zone2":0
         },
         meta=None
     )
 
 @router.post("/set-light")
 async def setlightvalue(req: LightRequest):
-    logger.info(req.zone1)
-    logger.info(req.zone2)
+    Lrep = LightRepository()
+    Lrep.set_light(req.zone1)
     return{
         "zone1":req.zone1,
         "zone2":req.zone2
