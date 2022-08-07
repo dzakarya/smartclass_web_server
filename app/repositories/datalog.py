a5520ec478b18b78737c99da0f3bcfb90c6a65b6
dzakarya <zakaria@koinworks.com>
2022-08-08 01:36:25 +0700

    fix date5

--- app/repositories/datalog.py
+++ app/repositories/datalog.py
@@ -6,9 +6,17 @@ def get_current_data(date : str, time : str ):
         for i in range(len(split_date)):
             if len(split_date[i]) < 2:
                 split_date[i] = "0"+split_date[i]
-                print(split_date[i])
-            if i < len(split_date):
+            if i < len(split_date)-1:
                 split_date[i] = split_date[i]+"-"
         date="".join(map(str,split_date))
     print(date)
+    if len(time) != 5 :
+        split_time = time.split("-")
+        for i in range(len(split_time)):
+            if len(split_time[i]) < 2:
+                split_time[i] = "0"+split_time[i]
+            if i < len(split_time)-1:
+                split_time[i] = split_date[i]+":"
+        split_time="".join(map(str,split_time))
+    print(time)
         
