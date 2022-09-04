34c2abc5d37d28a55d6fc73e35413225ae6ae7c7
dzakarya <zakaria@koinworks.com>
2022-09-04 10:23:36 +0700

    fix setmode when dark 4

--- app/worker/handler.py
+++ app/worker/handler.py
@@ -41,8 +41,8 @@ class BackgroundTasks(threading.Thread):
 
                 #condition when user set dark with automatic mode want to turn all the light without counter
 
+                logger.info("people detected")
                 if self.detector_thread.people_num != 0 or self.detector_thread2.people_num != 0:
-                    logger.info("people detected")
                     if mqtt.get_last_light() < highest_light_value:
                         mqtt.light = highest_light_value 
                         set_light(highest_light_value,highest_light_value)
